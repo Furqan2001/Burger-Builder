2a92bb83c9afa9d335dda52312120cd0820b17a6
Furqan2001 <58841647+Furqan2001@users.noreply.github.com>
2020-09-28 20:32:33 +0500

    Add Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
 import Logout from './containers/Auth/Logout/Logout';
-import Auth from './containers/Auth/Auth';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { checkAuthStatus } from './store/actions/index';
+
+const Checkout = React.lazy(() => import('./containers/Checkout/Checkout'));
+const Orders = React.lazy(() => import('./containers/Orders/Orders'));
+const Auth = React.lazy(() => import('./containers/Auth/Auth'));
 
 class App extends Component {
   
@@ -19,7 +20,7 @@ class App extends Component {
     let routes = (
       <Switch>
         <Route path='/' exact component={BurgerBuilder}/>
-        <Route path='/auth' component={Auth} />
+        <Route path='/auth' render={props => <Auth {...props} />} />
         <Redirect to='/' />
       </Switch>
     );
@@ -27,10 +28,10 @@ class App extends Component {
     if (this.props.isAuthenticated) {
       routes = (
         <Switch>
-          <Route path='/orders' component={Orders} />
-          <Route path='/checkout' component={Checkout} />
+          <Route path='/orders' render={props => <Orders {...props} />} />
+          <Route path='/checkout' render={props => <Checkout {...props} />} />
           <Route path='/logout' component={Logout} />
-          <Route path='/auth' component={Auth} />
+          <Route path='/auth' render={props => <Auth {...props} />} />
           <Route path='/' exact component={BurgerBuilder}/>
           <Redirect to='/' />
         </Switch>
@@ -40,7 +41,7 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          {routes}
+          <Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>
         </Layout>
       </div>
     );
